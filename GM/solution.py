dd95643e3a49fe43bfd670a5b86cd8451abaebd7
eggbread <gyeongmin251@gmail.com>
2021-01-22 11:29:00 +0900

    2021-01-22

--- GM/solution.py
+++ GM/solution.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import math
 
-def solution(name):
+def solution_01(name):
     """
     Problem : 프로그래머스 조이스틱
     Category : Greedy
@@ -36,6 +36,36 @@ def solution(name):
 
     return sum + min(back)
 
+def solution_02(num, k):
+    """
+    Problem : 프로그래머스 큰 수 만들기
+    Category : Greedy
+    Result : Pass
+    Description : 숫자 크기 비교일 때는 0이나 9같은 끝 값 예외처리로 시간 단축하기
+    """
+    length = len(num)
+    left = 0
+    right = k +1
+    result = ''
+    def findMax(arr):
+        maximum = '-1'
+        idx = 0
+        for i,x in enumerate(arr):
+            if x > maximum:
+                maximum = x
+                idx = i
+                if maximum == '9':break
+        return idx+left, str(maximum)
+    k = length - k
+    for i in range(k):
+        idx, maxValue = findMax(num[left:right])
+        left = idx + 1
+        right += 1
+        result += maxValue
+    return result
+
+
 if __name__ == '__main__':
-    name = "HAZ"
-    print(solution(name))
+    print(solution("1924",2))
+    print(solution("1231234",3))
+    print(solution("4177252841",4))
