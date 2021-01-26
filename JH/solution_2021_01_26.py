fb04bd939b193ebc95d68b1a91c72fcd07f7e3fe
whdgusdl48 <52066712+whdgusdl48@users.noreply.github.com>
2021-01-26 17:32:22 +0900

    Update solution_2021_01_26.py

--- JH/solution_2021_01_26.py
+++ JH/solution_2021_01_26.py
@@ -1,4 +1,5 @@
 def solution(s):
+    '중간 수 더하기'
     text_length = len(s)
     if text_length % 2 == 0:
         answer = s[text_length // 2 -1: text_length // 2 + 1]
@@ -8,6 +9,7 @@ def solution(s):
     return answer
 
 def solution(arr):
+    '같은 숫자는 싫어'
     a = []
     for i in arr:
         if a[-1:] == [i]: continue
@@ -17,6 +19,7 @@ def solution(arr):
     return answer
 
 def solution(arr, divisor):
+    '나누기 가능한 수 정렬'
     answer = []
     for i in arr:
         if i % divisor == 0:
@@ -27,6 +30,7 @@ def solution(arr, divisor):
     return answer
 
 def solution(a, b):
+    '두 수 사이의 합 더하기'
     min_val = min(a,b)
     length = max(a,b) - min_val
     answer = min_val
@@ -36,6 +40,7 @@ def solution(a, b):
     return answer
 
 def solution(strings, n):
+    '문자열 
     answer = []
     for i in range(len(strings)):
         strings[i] = strings[i][n] + strings[i]
