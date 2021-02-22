dc5df65ba1cab0bdf3a49ab96bff65d11903dd61
SoyeonBaek <soyeon.baek28@gmail.com>
2021-02-23 03:02:58 +0900

    (level3) 정수삼각형 - 동적계획법

---
+++ SY/Solution0223.java
@@ -0,0 +1,18 @@
+public class Solution0223 {
+	
+    public static int solution(int[][] triangle) {
+    	
+    	
+    	for(int i = triangle.length-1; i >0; i--) {
+    		for(int j = 0; j < triangle[i].length-1; j++) {
+    			triangle[i-1][j] = ((triangle[i][j] > triangle[i][j+1]?triangle[i][j]:triangle[i][j+1]) + triangle[i-1][j]);
+    		}
+    	}
+		return triangle[0][0];
+       
+    }
+    
+    
+}
+
+
