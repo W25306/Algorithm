46e092de23ecb75dd812748aa2061f00e9b95b93
eggbread <gyeongmin251@gmail.com>
2021-03-31 23:46:35 +0900

    scofe code festival

--- GM/solution.py
+++ GM/solution.py
@@ -442,6 +442,7 @@ def solution_21(n, a, b):
     Problem : 프로그래머스 예상 대진표
     Algorithm : Binary Search
     """
+
     def sol(s, e, a, b):
         if a <= (s + e) / 2 and b <= (s + e) / 2:  # Same Side
             if abs(a - b) == 1:
@@ -456,6 +457,7 @@ def solution_21(n, a, b):
         return answer
 
     return int(sol(0, n, a, b))
+
 
 def solution_22(a):
     """
@@ -476,14 +478,15 @@ def solution_22(a):
             r = item
         right.append(r)
     right.reverse()
-    for idx,item in enumerate(a):
-        if idx == 0 or idx == len(a)-1:
+    for idx, item in enumerate(a):
+        if idx == 0 or idx == len(a) - 1:
             continue
-        if max(item,left[idx-1],right[idx+1]) != item:
+        if max(item, left[idx - 1], right[idx + 1]) != item:
             answer += 1
         else:
             print(item)
     return answer
+
 
 def solution_23(routes):
     """
@@ -496,11 +499,12 @@ def solution_23(routes):
     for i, item in enumerate(routes[:-1]):
         if gate > item[1]:
             gate = item[1]
-        if gate < routes[i+1][0]:
+        if gate < routes[i + 1][0]:
             answer += 1
-            gate = routes[i+1][1]
+            gate = routes[i + 1][1]
 
     return answer
+
 
 def solution_24(n, words):
     """
@@ -508,28 +512,32 @@ def solution_24(n, words):
     """
     collection = set()
     steps = 0
-    for step in range(0,len(words),n):
+    for step in range(0, len(words), n):
         steps += 1
-        for i in range(step,step+n):
-            if i > 0 and words[i-1][-1] != words[i][0]:
-                return [i%n+1,steps]
+        for i in range(step, step + n):
+            if i > 0 and words[i - 1][-1] != words[i][0]:
+                return [i % n + 1, steps]
             if words[i] in collection:
-                return [i%n+1,steps]
+                return [i % n + 1, steps]
             else:
                 collection.add(words[i])
-    return [0,0]
+    return [0, 0]
+
 
 def solution_25(n):
     NOTATION = '0123'
+
     def change(N):
         q, r = divmod(N, 3)
         n = NOTATION[r]
         return change(q) + n if q else n
+
     answer = change(n)
     result = 0
-    for i,s in enumerate(answer):
+    for i, s in enumerate(answer):
         result += 3 ** i * int(s)
     return result
+
 
 def solution(N, stages):
     """
@@ -543,17 +551,449 @@ def solution(N, stages):
     count = Counter(stages)
     length = len(stages)
 
-    for i in range(1,N+1):
+    for i in range(1, N + 1):
         if length < 1:
-            heapq.heappush(heap,(0,i))
+            heapq.heappush(heap, (0, i))
         else:
-            heapq.heappush(heap, (-(count[i]/length),i))
+            heapq.heappush(heap, (-(count[i] / length), i))
         length -= count[i]
     while heap:
         answer.append(heapq.heappop(heap)[1])
     return answer
 
+
+def solution(genres, plays):
+    class album:
+        def __init__(self, g, p):
+            self.genre = g
+            self.play = p
+
+    from collections import defaultdict
+    answer = []
+    library = defaultdict(album)
+    for genre, play in zip(genres, plays):
+        print(genre, play)
+        if genre in library:
+            pass
+        else:
+            newAlbum = album(genre, play)
+            if not answer:
+                answer.append(newAlbum)
+            else:
+                pass
+
+    return answer
+
+
+def sol1():
+    from collections import deque
+    n = int(input())
+    queue = deque(maxlen=2)
+    for i in range(n):
+        time = input().split(" ~ ")
+        if not queue:
+            queue.appendleft(time[0])
+            queue.append(time[1])
+        else:
+            left = queue.popleft()
+            queue.appendleft(max(left, time[0]))
+            right = queue.pop()
+            queue.append(min(right, time[1]))
+    print(queue.popleft() + " ~ " + queue.pop())
+
+
+def sol2():
+    count = [0, 0, 0, 2, 3]
+    for i in range(5, 51):
+        count.append(count[i - 1] + count[i - 2])
+    n = int(input())
+    path = input().split("0")
+    answer = 0
+    if path[0] == "11":
+        answer += 1
+    for p in path:
+        answer += count[len(p)]
+    print(answer)
+
+
+def sol22():
+    n = int(input())
+    path = input()
+    stack = [0]
+    answer = 0
+    while stack:
+        item = stack.pop()
+        if item == n - 1:
+            answer += 1
+        if item + 1 < n:
+            if path[item + 1] == '1':
+                stack.append(item + 1)
+        if item + 2 < n:
+            if path[item + 2] == '1':
+                stack.append(item + 2)
+    print(answer)
+
+
+def sol3():
+    def split(arr, left, right, top, bottom):
+        sumValue = 0
+        for i in range(top, bottom):
+            sumValue += sum(arr[i][left:right])
+        return sumValue
+
+    n = int(input())
+    room = []
+    total = 0
+    for i in range(n):
+        temp = list(map(int, input()))
+        total += sum(temp)
+        room.append(temp)
+    stage = [total]
+    for i in range(2, n + 1):
+        temp = 0
+        for col in range(n - i + 1):
+            for row in range(n - i + 1):
+                if split(room, row, row + i, col, col + i) == i ** 2:
+                    temp += 1
+        stage.append(temp)
+    print("total:", sum(stage))
+    for i, v in enumerate(stage):
+        if v:
+            print("size[" + str(i + 1) + "]: " + str(v))
+
+
+def sol4():
+    import heapq
+    preference = list(map(float, input().split(" ")))
+    col, row = list(map(int, input().split(" ")))
+    library = []
+    subject = []
+    for c in range(col):
+        library.append(input())
+    for c in range(col):
+        subject.append(input())
+    heapY = []
+    heapO = []
+    for i, col in enumerate(zip(library, subject)):
+        for j, row in enumerate(zip(col[0], col[1])):
+            if row[0] == 'Y':
+                heapq.heappush(heapY,
+                               ((-preference[ord(row[1]) - 65], i, j), (row[1], preference[ord(row[1]) - 65], i, j)))
+            if row[0] == 'O':
+                heapq.heappush(heapO,
+                               ((-preference[ord(row[1]) - 65], i, j), (row[1], preference[ord(row[1]) - 65], i, j)))
+    while heapY:
+        out = heapq.heappop(heapY)[1]
+        print(out[0], out[1], out[2], out[3])
+    while heapO:
+        out = heapq.heappop(heapO)[1]
+        print(out[0], out[1], out[2], out[3])
+
+
+def sol5():
+    row, col = list(map(int, input().split(" ")))
+    room = []
+    answer = 0
+
+    for c in range(col):
+        room.append(list(map(str, input())))
+    startPoint = []
+    for r in range(row):
+        if room[0][r] == 'c':
+            startPoint.append([0, r, 0])
+    answer = []
+    for start in startPoint:
+        check = [[False] * row for _ in range(col)]
+        stack = [start]
+        while stack:
+            item = stack.pop()
+            check[item[0]][item[1]] = True
+            if item[0] == col - 1:
+                answer.append(item[2])
+                continue
+            if room[item[0] + 1][item[1]] == '.':
+                stack.append([item[0] + 1, item[1], item[2]])
+            if room[item[0] + 1][item[1]] == 'x':
+                if item[1] < row - 1:
+                    if not check[item[0]][item[1] + 1]:
+                        stack.append([item[0], item[1] + 1, item[2] + 1])
+                if item[1] > 0:
+                    if not check[item[0]][item[1] - 1]:
+                        stack.append([item[0], item[1] - 1, item[2] + 1])
+    print(min(answer))
+
+
+def sol6():
+    row, col = list(map(int, input().split(" ")))
+    room = []
+    for c in range(col):
+        room.append(list(map(int, input().split(" "))))
+    for c in range(col):
+        for r in range(row):
+            if c == 0 and r == 0:
+                continue
+            elif c == 0:
+                room[c][r] += room[c][r - 1]
+            elif r == 0:
+                room[c][r] += room[c - 1][r]
+            else:
+                room[c][r] += max(room[c][r - 1], room[c - 1][r])
+    print(room[col - 1][row - 1])
+
+
+def solu1():
+    from datetime import datetime
+    num, sumTime = input().split(" ")
+    musicList = []
+    count = 0
+    startPoint = 0
+    pt = datetime.strptime(sumTime, '%H:%M:%S')
+    sumTime = pt.second + pt.minute * 60 + pt.hour * 3600
+    for _ in range(int(num)):
+        minute, second = map(int, input().split(":"))
+        musicList.append(minute * 60 + second)
+    for i, v in enumerate(musicList):
+        sumValue = 0
+        cnt = 0
+        idx = i
+        while sumValue < sumTime and idx < int(num):
+            sumValue += musicList[idx]
+            cnt += 1
+            idx += 1
+        if cnt > count:
+            count = cnt
+            startPoint = i + 1
+    print(count, startPoint)
+
+
+def solu2():
+    num = int(input())
+    city_list = []
+    city_set = set()
+    parent = {}
+    rank = {}
+
+    def make(v):
+        parent[v] = v
+        rank[v] = 0
+
+    def find(v):
+        if parent[v] != v:
+            parent[v] = find(parent[v])
+        return parent[v]
+
+    def union(a, b):
+        r1 = find(a)
+        r2 = find(b)
+
+        if r1 != r2:
+            if rank[r1] > rank[r2]:
+                parent[r2] = r1
+            else:
+                parent[r1] = r2
+
+                # if rank[r1] == rank[r2]:
+                #     rank[r2] += 1
+
+    def check(city_set, city_list):
+        cost = 0
+        for c in city_set:
+            make(c)
+        edges = city_list
+        edges.sort(key=lambda x: int(x[2]))
+        for edge in edges:
+            a, b, v = edge
+            if find(a) != find(b):
+                union(a, b)
+                cost += int(v)
+        return cost
+
+    for _ in range(num):
+        path = input().split()
+        city_list.append(path)
+        city_set.add(path[0])
+        city_set.add(path[1])
+    result = check(city_set, city_list)
+    print(result)
+
+
+def solu3():
+    from collections import deque
+    def find(a, queue):
+        for i, v in enumerate(board[a]):
+            if v:
+                queue.append(i)
+
+    num, ask = map(int, input().split())
+    board = [[False] * num for _ in range(num)]
+    board = board
+    for _ in range(num - 1):
+        a, b = map(int, input().split())
+        board[a - 1][b - 1] = True
+    for _ in range(ask):
+        a, b = map(int, input().split())
+        queue = deque([a-1])
+        found = False
+        while queue:
+            item = queue.popleft()
+            if item == b - 1:
+                found = True
+                break
+            find(item, queue)
+        print('yes') if found else print('no')
+
+
+def solu4():
+    length = int(input())
+    index_list = []
+    for _ in range(length):
+        index_list.append(input())
+    num = int(input())
+    for _ in range(num):
+        type = input()
+        cnt = 0
+        for item in index_list:
+            if type in item:
+                cnt += 1
+        print(cnt)
+
+
+def solu42():
+    from collections import defaultdict
+    from itertools import combinations
+    length = int(input())
+    index_list = defaultdict(int)
+    for _ in range(length):
+        key = input()
+        for i in range(2, min(len(key) + 1, 21)):
+            combi = combinations(key, i)
+            for item in combi:
+                item = "".join(item)
+                val = index_list[item]
+                if val == 0:
+                    index_list[item] = 1
+                else:
+                    index_list[item] = val + 1
+    count = int(input())
+    for _ in range(count):
+        print(index_list[input()])
+
+def solu43():
+    def search(pattern, text):
+        m = len(pattern)
+        n = len(text)
+        lps = [0]*m
+        compute(pattern, lps)
+        i = 0
+        j = 0
+        while i<n:
+            if pattern[j] == text[i]:
+                i += 1
+                j += 1
+            elif pattern[j] != text[i]:
+                if j != 0:
+                    j = lps[j-1]
+                else:
+                    i +=1
+            if j == m:
+                return True
+        return False
+
+    def compute(pattern, lps):
+        length = 0
+        i = 1
+        while i < len(pattern):
+            if pattern[i] == pattern[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length-1]
+                else:
+                    lps[i] = 0
+                    i += 1
+
+    def find(parent, pattern):
+        parentSize = len(parent)
+        patternSize = len(pattern)
+        parentHash = 0
+        patternHash = 0
+        power = 1
+        for i in range(parentSize-patternSize+1):
+            if i == 0:
+                for j in range(patternSize):
+                    parentHash += ord(parent[patternSize - 1-j])*power
+                    patternHash += ord(pattern[patternSize-1-j])*power
+                    if j < patternSize-1:
+                        power *=2
+            else:
+                parentHash = 2*(parentHash-ord(parent[i-1])*power)+ord(parent[patternSize-1+i])
+            if parentHash == patternHash:
+                found = True
+                for j in range(patternSize):
+                    if parent[i+j]!=pattern[j]:
+                        found = False
+                        break
+                if found:
+                    return True
+                else:
+                    return False
+
+    def getPartialMatch(N):
+        m = len(N)
+
+        pi = [0] * m
+        begin = 1
+        matched = 0
+        while begin + matched < m:
+            if N[begin + matched] == N[matched]:
+                matched += 1
+                pi[begin + matched - 1] = matched
+            else:
+                if matched == 0:
+                    begin += 1
+                else:
+                    begin += matched - pi[matched - 1]
+                    matched = pi[matched - 1]
+        return pi
+
+    def kmpSearch(H, N):
+        n = len(H)
+
+        m = len(N)
+        ret = []
+        pi = getPartialMatch(N)
+        begin = 0
+        matched = 0
+        while begin <= n - m:
+            if matched < m and H[begin + matched] == N[matched]:
+                matched += 1
+                if matched == m:
+                    ret.append(begin)
+            else:
+                if matched == 0:
+                    begin += 1
+                else:
+                    begin += matched - pi[matched - 1]
+                    matched = pi[matched - 1]
+        return ret
+
+    length = int(input())
+    index_list = []
+    for _ in range(length):
+        index_list.append(input())
+    num = int(input())
+    for _ in range(num):
+        key = input()
+        cnt = 0
+        for item in index_list:
+            if kmpSearch(item, key):
+                cnt += 1
+        print(cnt)
+
 if __name__ == '__main__':
-    test = [4,4,4,4,4]
-    N = 4
-    print(solution(N,test))
+    solu43()
+    # test = ["classic", "pop", "classic", "classic", "pop"]
+    # N = [500, 600, 150, 800, 2500]
+    # print(solution(test,N))
